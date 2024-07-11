4b9a7cfc99b3a90ebe8adf60084cb89ea7304c23
Maylis Maria <maylis.maria15@gmail.com>
2024-07-11 20:49:34 -0300

    Path Params and Exception Handling

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/math/SimpleMath.java
@@ -0,0 +1,38 @@
+package br.com.erudio.math;
+
+public class SimpleMath {
+	
+	public Double sum (Double numberOne, Double numberTwo) {
+	       
+		return  numberOne  +  numberTwo;
+	}
+	
+	
+	public Double subtract(Double numberOne, Double numberTwo) {
+	       
+		return  numberOne  -  numberTwo;
+	}
+		
+	
+	public Double multiplication (Double numberOne, Double numberTwo) {
+	       
+		return  numberOne  *  numberTwo;
+	}
+	
+	
+    public Double divison (Double numberOne, Double numberTwo) {
+    	
+		return  numberOne  /  numberTwo;
+    }
+			
+    
+	public Double squareRoot (Double number) {
+				
+
+		return Math.sqrt(number);
+			
+	}
+	
+}
+
+
